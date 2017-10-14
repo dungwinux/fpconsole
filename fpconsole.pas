a4bec9ab8130e14e826a82e611d296148060365f
Nguyễn Hoàng Dương <novakglow@gmail.com>
2017-10-14 18:21:15 +0700

    Update fpconsole.pas
    Reformatted the file + Add comments

--- fpconsole.pas
+++ fpconsole.pas
@@ -1,17 +1,18 @@
-uses crt, SysUtils;
-var
+USES CRT, SysUtils;
+VAR
     TEMPFOLDER, Build, dir, fname: AnsiString;
     m: text;
 
-procedure InitBuild();
-var s: string;
-begin
-    s:={$I %DATE%}+'-'+{$I %TIME%};
-	while pos('/',s) <> 0 do delete(s,pos('/',s),1);
-	while pos(':',s) <> 0 do delete(s,pos(':',s),1);
-    delete(s,1,2);delete(s,length(s)-1,2);
-    Build:=s;
-end;
+Procedure InitBuild();
+Var s: String;
+Begin
+    s := {$I %DATE%}+'-'+{$I %TIME%};
+    While pos('/',s) <> 0 do Delete(s, pos('/', s), 1);
+    While pos(':',s) <> 0 do Delete(s, pos(':', s), 1);
+    Delete(s, 1, 2);
+    Delete(s, length(s) - 1, 2);
+    Build := s;
+End;
 
 Procedure Help;
 Begin
@@ -19,16 +20,16 @@ Begin
     Writeln('To make it easy, you can directly throw input as the argument or write it in a file');
     Writeln('Sometimes, when there is an error or an infinite loop and the program exited improperly, you can review the code in %TMP%\FPConsole folder');
     Writeln('All FPConsole Switch:');
-    Writeln('-c     :   Clear TEMP');
-    Writeln('-fs    :   Read the whole file in formatted type (.pas)');
-    Writeln('-f     :   Read text file with only Function and Procedure');
-    Writeln('-h     :   Show this help');
+    Writeln('-c  :   Clear TEMP');
+    Writeln('-fs :   Read the whole file in formatted type (.pas)');
+    Writeln('-f  :   Read text file with only Function and Procedure');
+    Writeln('-h  :   Show this help');
     Writeln('FPConsole is an Open-Source Program. Github: fpconsole');
 End;
 
-Function Create: boolean;
-var tmp: AnsiString;
+Function Create: Boolean;
 // Generate source file to compile
+Var tmp: AnsiString;
 Begin
     Randomize;
     Str(random(100000), fname);
@@ -44,7 +45,7 @@ End;
 
     // Pass the code to the temp source file
 Procedure Input(s: string);
-VAR f: text;
+Var f: text;
 Begin
     Assign(f, s);
     {$I-} Reset(f); {$I+}
@@ -58,46 +59,47 @@ Begin
 End;
 
 Procedure ReadDat;
-VAR 
-    i: byte;
-    t: string;
+// Write code to source file
+Var
+    i: Byte;
+    t: String;
 Begin
     If ParamStr(1) = '-fs' then Input(ParamStr(2))
     else Begin
-        Write(m, 'uses ');
-        Input('unit.dat');  // Get unit
-        Writeln(m, 'crt;');
-        Writeln(m, #13#10, 'type', #13#10, 'Int = Integer;');
-        Input('type.dat');  // Get type
-        Writeln(m, #13#10, 'const', #13#10, '_Default=', #39, 'FPConsole', #39, ';');
-        Input('const.dat'); // Get const
-        Writeln(m, #13#10, 'var', #13#10, '_nuStr:string;', #13#10, '_nInt:integer;', #13#10, '_nReal:real;', #13#10, '_nText:text;');
-        Input('var.dat');   // Get var
-        Writeln(m, #13#10, 'begin');
-        Case ParamStr(1) of
+         Write(m, 'uses ');
+         Input('unit.dat');  // Get unit
+         Writeln(m, 'crt;');
+         Writeln(m, #13#10, 'type', #13#10, 'Int = Integer;');
+         Input('type.dat');  // Get type
+         Writeln(m, #13#10, 'const', #13#10, '_Default=', #39, 'FPConsole', #39, ';');
+         Input('const.dat'); // Get const
+         Writeln(m, #13#10, 'var', #13#10, '_nuStr:string;', #13#10, '_nInt:integer;', #13#10, '_nReal:real;', #13#10, '_nText:text;');
+         Input('var.dat');   // Get var
+         Writeln(m, #13#10, 'begin');
+         Case ParamStr(1) of
             '-f' :  Input(ParamStr(2));
-            ''  :   Help;
-            else For i := 1 to ParamCount do Writeln(m, ParamStr(i));
-        End;
-        Write(m, 'end.');
+            ''   :  Help;
+         else For i := 1 to ParamCount do Writeln(m, ParamStr(i));
+         End;
+    Write(m, 'end.');
     End;
     Close(m); 
 End;
 
-Function Get: boolean;
-VAR FileDat: TSearchRec;
+Function Get: Boolean;
+Var FileDat: TSearchRec;
 Begin
     {$IFDEF MSWINDOWS}
     Get := DirectoryExists('C:\FPC\');
     If Get then Begin
-                If FindFirst('C:\FPC\*', faDirectory, FileDat) = 0 then
-                    Repeat
-                        dir := FileDat.Name;
-                    Until FindNext(FileDat) <> 0;
+                If FindFirst('C:\FPC\*', faDirectory, FileDat) = 0 
+                    then Repeat
+                         dir := FileDat.Name;
+                         Until FindNext(FileDat) <> 0;
                 FindClose(FileDat);
                 If dir = '..' then Get := False
                     else dir := 'C:\FPC\' + dir + '\bin\i386-win32\fpc.exe';
-    End;
+                End;
     Get := Get and FileExists(dir);
     {$ENDIF}
 
@@ -109,13 +111,13 @@ Begin
 End;
 
 Function Find(s: string): boolean;
-VAR 
+Var
     FileDat: TSearchRec;
     b: boolean;
 Begin
     s := s + {$IFDEF MSWINDOWS}'\'{$ENDIF} {$IFDEF LINUX}'/'{$ENDIF};
     Find := DirectoryExists(s);
-    If Find then begin
+    If Find then Begin
         If FindFirst(s + '*', faAnyFile, FileDat) = 0 then
             Repeat 
                 b := (Find and (FileDat.Name = {$IFDEF MSWINDOWS}'fpc.exe'{$ENDIF} {$IFDEF LINUX}'fpc'{$ENDIF})) 
@@ -126,7 +128,7 @@ Begin
 End;
 
 Function SysFind:boolean;
-VAR s: AnsiString;
+Var s: AnsiString;
 Begin
     s := GetEnvironmentVariable('PATH') + {$IFDEF MSWINDOWS}';'{$ENDIF} {$IFDEF LINUX}':'{$ENDIF};
     Repeat
@@ -138,11 +140,12 @@ Begin
 End;
 
 Procedure Execute;
-var exitcode: integer;
+Var exitcode: integer;
 Begin
     Writeln('FPC Dir:', dir);
     ReadDat;
-    if (ParamStr(1)<>'') then begin
+    If (ParamStr(1)<>'') then 
+    Begin
         {$IFDEF MSWINDOWS}ExecuteProcess(dir, ['-v0', fname], []);{$ENDIF}
         {$IFDEF LINUX}ExecuteProcess('/bin/bash', ['-c', 'fpc ' + fname + ' &>/dev/null']);{$ENDIF}
         DeleteFile(fname + '.pas');
@@ -150,44 +153,43 @@ Begin
         Writeln('[OUTPUT]');
         Assign(m, fname {$IFDEF MSWINDOWS}+ '.exe'{$ENDIF});
         {$I-} Reset(m); {$I+}
-        If IOResult = 0 then begin
-            Close(m);
-            DeleteFile(fname + '.o');
-            exitcode := ExecuteProcess(fname {$IFDEF MSWINDOWS}+ '.exe'{$ENDIF}, '', []);
-            DeleteFile(fname {$IFDEF MSWINDOWS}+ '.exe'{$ENDIF});
-            writeln;
-            writeln('---------------------');
-            writeln('Process Exited with Exit code ', exitcode);
-        End
+        If IOResult = 0 then 
+            Begin
+                Close(m);
+                DeleteFile(fname + '.o');
+                exitcode := ExecuteProcess(fname {$IFDEF MSWINDOWS}+ '.exe'{$ENDIF}, '', []);
+                DeleteFile(fname {$IFDEF MSWINDOWS}+ '.exe'{$ENDIF});
+                Writeln;
+                Writeln('---------------------');
+                Writeln('Process terminated with exit code ', exitcode);
+            End
         else Writeln('COMPILE ERROR');
-    end;
+    End;
 End;
 
-// Credit goes to David Heffernan on Stack Overflow
-// https://stackoverflow.com/questions/16336761/delete-directory-with-non-empty-subdirectory-and-files?answertab=votes#tab-top
-procedure DeleteDir(const DirName: Ansistring);
-var
-  Path: string;
-  F: TSearchRec;
-begin
+Procedure DeleteDir(const DirName: Ansistring);
+// Delete program's temporary directory (might be non-empty) on Windows
+Var
+    Path: string;
+    F: TSearchRec;
+Begin
     Path:= DirName + '\*.*';
-    if FindFirst(Path, faAnyFile, F) = 0 then begin
-        repeat
-            if (F.Attr and faDirectory <> 0) then begin
-                if (F.Name <> '.') and (F.Name <> '..') then begin
-                    DeleteDir(DirName + '\' + F.Name);
-                end;
-            end
+    If FindFirst(Path, faAnyFile, F) = 0 then 
+        Repeat
+            If (F.Attr and faDirectory <> 0) then 
+                Begin
+                    If (F.Name <> '.') and (F.Name <> '..') then
+                        DeleteDir(DirName + '\' + F.Name);
+                End
             else
                 DeleteFile(DirName + '\' + F.Name);
-        until FindNext(F) <> 0;
-    end;
+        Until FindNext(F) <> 0;
     FindClose(F);
     RemoveDir(DirName);
-end;
+End;
 
 Procedure Clear;
-var tmp: AnsiString;
+Var tmp: AnsiString;
 Begin
     tmp := TEMPFOLDER;
     If DirectoryExists(tmp) then Begin
@@ -198,12 +200,12 @@ Begin
     writeln('TEMP Folder Removed!');
 End;
 
-begin
-    Clrscr;InitBuild;
+BEGIN
+    Clrscr; InitBuild;
     TEMPFOLDER := {$IFDEF MSWINDOWS}GetEnvironmentVariable('TEMP') + '\FPConsole'{$ENDIF} {$IFDEF LINUX}'/tmp/FPConsole'{$ENDIF};
-    Writeln('FPConsole ',Build,' - Created by Winux8YT3');
-    writeln('TEMP Folder: ', TEMPFOLDER);
+    Writeln('FPConsole ', Build, ' - Created by Winux8YT3');
+    Writeln('TEMP Folder: ', TEMPFOLDER);
     If ParamStr(1) = '-h' then Help
-    else if ParamStr(1) = '-c' then Clear
-    else if Create and (Get or SysFind) then Execute else Writeln('FPC NOT FOUND');
-end.
+        else If ParamStr(1) = '-c' then Clear
+        else If Create and (Get or SysFind) then Execute else Writeln('FPC NOT FOUND.');
+END.
